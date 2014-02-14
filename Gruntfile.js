b5034a7ee6eb11f17fd413b5b253688c2e4a1d88
Evan Lucas <evan.lucas@hattiesburgclinic.com>
2014-02-14 10:16:15 -0600

    Make spec the default reporter

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    
+
     cafemocha: {
       main: {
         src: 'tests/test.js',
         options: {
           ui: 'bdd',
-          reporter: grunt.option('reporter') || 'nyan',
+          reporter: grunt.option('reporter') || 'spec',
           colors: true
         }
       }
     }
   })
-  
+
   grunt.loadNpmTasks('grunt-cafe-mocha')
-  
+
   grunt.registerTask('default', ['cafemocha'])
 }
